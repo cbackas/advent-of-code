e0fa00892e17ed23492584f2a664c28199573409
cbackas <zgibson@live.com>
2023-12-04 23:02:37 -0600

    improve day 4 a lil

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -58,48 +58,42 @@ struct Card {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let lines = input.lines().collect_vec();
+    let mut cards = input
+        .lines()
+        .map(|line| {
+            let chars = line.chars();
 
-    let mut cards: Vec<Card> = Vec::with_capacity(lines.len());
+            let winning_numbers: String = chars
+                .clone()
+                .skip_while(|c| !c.eq(&':'))
+                .skip(1)
+                .take_while(|c| !c.eq(&'|'))
+                .collect();
+            let winning_numbers = winning_numbers.trim();
+            let winning_numbers = winning_numbers
+                .split(" ")
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect_vec();
 
-    let mut i = 0;
-    while i < lines.len() {
-        let chars = lines[i].chars();
+            let potential_numbers: String = chars.skip_while(|c| !c.eq(&'|')).skip(2).collect();
+            let potential_numbers = potential_numbers.trim();
+            let potential_numbers = potential_numbers
+                .split(" ")
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect_vec();
 
-        let winning_numbers: String = chars
-            .clone()
-            .skip_while(|c| !c.eq(&':'))
-            .skip(1)
-            .take_while(|c| !c.eq(&'|'))
-            .collect();
-        let winning_numbers = winning_numbers.trim();
-        let winning_numbers = winning_numbers
-            .split(" ")
-            .filter(|s| !s.is_empty())
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect_vec();
-
-        let potential_numbers: String = chars.skip_while(|c| !c.eq(&'|')).skip(2).collect();
-        let potential_numbers = potential_numbers.trim();
-        let potential_numbers = potential_numbers
-            .split(" ")
-            .filter(|s| !s.is_empty())
-            .map(|s| s.parse::<u32>().unwrap())
-            .collect_vec();
-
-        let mut matches = 0;
-        for number in winning_numbers {
-            if potential_numbers.contains(&number) {
-                matches += 1;
+            let mut matches = 0;
+            for number in winning_numbers {
+                if potential_numbers.contains(&number) {
+                    matches += 1;
+                }
             }
-        }
 
-        let card = Card { matches, count: 1 };
-
-        cards.push(card);
-
-        i += 1;
-    }
+            Card { matches, count: 1 }
+        })
+        .collect_vec();
 
     let mut i = 0;
     while i < cards.len() {
